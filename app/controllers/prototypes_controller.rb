09ad97201e49b26bdf2de5516d7c847fc0e07fcb
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-10-15 17:17:38 +0900

    投稿機能を実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,8 +3,23 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    #@user = User.find(params[:user_id])
+    #@prototype = @user.prototypes.new(prototype_params)
+    @prototype = Prototype.create(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
   end
 end
