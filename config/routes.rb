ce823d6b462412294f032413a9b84747d70fe790
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-10-16 12:33:46 +0900

    commentsコントローラの作成とcreateアクション、ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'comments/create'
   devise_for :users
   root to: "prototypes#index"
-  resources :prototypes, only: [:new,:create,:index,:show,:edit,:update,:destroy]
+  resources :prototypes, only: [:new,:create,:index,:show,:edit,:update,:destroy] do
+    resources :comments, only: :create
+  end
   
 end
