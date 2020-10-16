e1bc3260562adada34b6cc0a167b99ec407616e0
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-10-16 17:03:04 +0900

    ユーザー詳細ページを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'comments/create'
   devise_for :users
   root to: "prototypes#index"
   resources :prototypes, only: [:new,:create,:index,:show,:edit,:update,:destroy] do
     resources :comments, only: :create
   end
+
+  resources :users, only: :show
   
 end
